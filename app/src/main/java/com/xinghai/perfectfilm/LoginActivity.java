ac3b8b33c10daec063e8c88114f7fee9aacbb780
yuanbaoyu <yuanbaoyu@wangchengiMac.local>
2017-05-02 18:18:44 +0800

    完成登录、注册界面

---
+++ app/src/main/java/com/xinghai/perfectfilm/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.xinghai.perfectfilm;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Explode;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fab;
+    private Button bt_go;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        bt_go = (Button) findViewById(R.id.bt_go);
+        bt_go.setOnClickListener(this);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab:
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options =
+                            ActivityOptions.makeSceneTransitionAnimation(this, fab, fab.getTransitionName());
+                    startActivity(new Intent(this, RegisterActivity.class), options.toBundle());
+                } else {
+                    startActivity(new Intent(this, RegisterActivity.class));
+                }
+                break;
+            case R.id.bt_go:
+                Explode explode = new Explode();
+                explode.setDuration(500);
+                getWindow().setExitTransition(explode);
+                getWindow().setEnterTransition(explode);
+                ActivityOptionsCompat oc2 = ActivityOptionsCompat.makeSceneTransitionAnimation(this);
+                Intent i2 = new Intent(this,MainActivity.class);
+                startActivity(i2, oc2.toBundle());
+                break;
+        }
+    }
+}
